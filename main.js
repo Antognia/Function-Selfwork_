85f916bb7eaa417b9231e27ac80a6d214c062cc7
Antognia <168127637+Antognia@users.noreply.github.com>
2024-06-28 18:37:42 +0200

    Esercizio Array 1 Corretto

--- main.js
+++ main.js
@@ -21,13 +21,12 @@ console.log(palindromo(frase));
 
 const numeri = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4];
 
-function ordineDecrescente(num) {
+let crescente = numeri.sort((a,b) => a - b);
+console.log(crescente);
     
-    let crescente = num.sort();
-
-    let decrescente = crescente.reverse();
-
+let decrescente = numeri.sort( (a,b) => b - a);
 console.log(decrescente);
+
 
 
 
